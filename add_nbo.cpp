4181dd0976cce59b4ce5c3e07e30a8d788ac1fdb
ldy4500 <66829196+ldy4500@users.noreply.github.com>
2020-09-16 22:23:18 +0900

    Update add_nbo.cpp

--- add_nbo.cpp
+++ add_nbo.cpp
@@ -7,6 +7,6 @@ int add_nbo(uint32_t x1, uint32_t x2){
 	uint32_t result;
 	result = x1 + x2;
 
-	printf("%d(%x) + %d(%x) = %d(%x)", x1, x1, x2, x2, result, result);
+	printf("%d(%x) + %d(%x) = %d(%x)\n", x1, x1, x2, x2, result, result);
 	return 0;
 }
